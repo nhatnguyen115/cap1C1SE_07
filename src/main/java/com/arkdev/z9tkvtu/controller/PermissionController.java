45773a9cac2c0a54e7928a2c1986811cdf665f53
nameless1908 <huytech1908@gmail.com>
2025-03-17 17:10:31 +0700

    add security config, per filter; user, role, permission, auth controller

---
+++ src/main/java/com/arkdev/z9tkvtu/controller/PermissionController.java
@@ -0,0 +1,70 @@
+package com.arkdev.z9tkvtu.controller;
+
+import com.arkdev.z9tkvtu.dto.Request.PermissionRequest;
+import com.arkdev.z9tkvtu.dto.Response.ResponseData;
+import com.arkdev.z9tkvtu.dto.Response.ResponseError;
+import com.arkdev.z9tkvtu.service.PermissionService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequestMapping("/permission")
+@RequiredArgsConstructor
+public class PermissionController {
+    PermissionService permissionService;
+
+    @GetMapping("")
+    public ResponseData<?> getPermissions() {
+        try {
+            return new ResponseData<>(HttpStatus.OK.value(),
+                    "Get Permissions Successfully", permissionService.getPermissions());
+        } catch (Exception e) {
+            return new ResponseError<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Get Permissions failed");
+        }
+    }
+
+    @GetMapping("/{permissionId}")
+    public ResponseData<?> getPermission(@PathVariable Integer permissionId) {
+        try {
+            return new ResponseData<>(HttpStatus.OK.value(),
+                    "Get Permission Successfully", permissionService.getPermission(permissionId));
+        } catch (Exception e) {
+            return new ResponseError<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Get Permission failed");
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseData<?> addPermission(@RequestBody PermissionRequest request) {
+        try {
+            permissionService.addPermission(request);
+            return new ResponseData<>(HttpStatus.OK.value(), "Permission Added Successfully");
+        } catch (Exception e) {
+            return new ResponseError<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Permission could not be added");
+        }
+    }
+
+    @PutMapping("/update/{permissionId}")
+    public ResponseData<?> updatePermission(@PathVariable Integer permissionId,
+                                            @RequestBody PermissionRequest request) {
+        try {
+            permissionService.updatePermission(permissionId, request);
+            return new ResponseData<>(HttpStatus.OK.value(), "Permission Updated Successfully");
+        } catch (Exception e) {
+            return new ResponseError<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Permission could not be updated");
+        }
+    }
+
+    @DeleteMapping("/delete/{permissionId}")
+    public ResponseData<?> deletePermission(@PathVariable Integer permissionId) {
+        try {
+            permissionService.deletePermission(permissionId);
+            return new ResponseData<>(HttpStatus.OK.value(), "Permission Deleted Successfully");
+        } catch (Exception e) {
+            return new ResponseError<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Permission could not be deleted");
+        }
+    }
+}
