c7b7c762c80f2981ca37e8a0ee2576b9a5bf27e8
namnt2 <namnt2@unit.com.vn>
2025-04-11 10:33:02 +0700

    [NamNT] change active header

--- src/layouts/components/MenuNav.tsx
+++ src/layouts/components/MenuNav.tsx
@@ -11,7 +11,13 @@ export const MenuNav = ({ menuItems }: Props) => {
   const location = useLocation();
   const [openDropdown, setOpenDropdown] = useState<number | null>(null);
 
-  const isActive = (url: string) => location.pathname === url;
+  const isActive = (item: MenuItem): boolean => {
+    if (location.pathname === item.url) return true;
+    if (item.children) {
+      return item.children.some((child) => location.pathname === child.url);
+    }
+    return false;
+  };
 
   return (
     <nav className="flex space-x-6 relative">
@@ -29,7 +35,7 @@ export const MenuNav = ({ menuItems }: Props) => {
             <Link
               to={item.url}
               className={`hidden sm:block px-2 py-1 rounded transition duration-200 transform ${
-                isActive(item.url)
+                isActive(item)
                   ? "text-orange-600 text-xl"
                   : "text-gray-600 hover:text-orange-500 hover:-translate-y-0.5"
               }`}
