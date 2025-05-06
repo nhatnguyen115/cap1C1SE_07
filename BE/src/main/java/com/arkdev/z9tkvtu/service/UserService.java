1c71768db6d34caacf87a6ebe0f1b1c61e7196e1
Võ Duy Vũ <voduyvu1206@gmail.com>
2025-05-06 15:49:20 +0700

    [BE] update source

--- BE/src/main/java/com/arkdev/z9tkvtu/service/UserService.java
+++ BE/src/main/java/com/arkdev/z9tkvtu/service/UserService.java
@@ -9,15 +9,30 @@ import com.arkdev.z9tkvtu.model.UserLoginData;
 import com.arkdev.z9tkvtu.repository.RoleRepository;
 import com.arkdev.z9tkvtu.repository.UserLoginDataRepository;
 import com.arkdev.z9tkvtu.util.RoleType;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
 import jakarta.annotation.PostConstruct;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -29,12 +44,95 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private Role userRole;
 
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String googleClientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String googleClientSecret;
+
+    @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
+    private String googleRedirectUri;
+
     @PostConstruct
     private void init() {
         userRole = roleRepository.findByRoleType(RoleType.USER)
                 .orElseThrow(() -> new RuntimeException("Role not found"));
     }
 
+    public String generateUrl(String type) {
+        String url = "";
+        if ("google".equalsIgnoreCase(type)) {
+            GoogleAuthorizationCodeRequestUrl codeRequestUrl = new GoogleAuthorizationCodeRequestUrl(
+                    googleClientId,
+                    googleRedirectUri,
+                    Arrays.asList("email", "profile", "openid")
+            );
+            url = codeRequestUrl.build();
+        } else if ("facebook".equalsIgnoreCase(type)) {
+            url = UriComponentsBuilder
+                    .fromUriString("https://graph.facebook.com/me")
+                    .queryParam("scope", "email,public_profile")
+                    .queryParam("response_type", "code")
+                    .build()
+                    .toUriString();
+        }
+        return url;
+    }
+
+    public Map<String, Object> authenticateAndFetchProfile(String code, String loginType) throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        String accessToken;
+        //Gson gson = new Gson();
+
+        switch (loginType.toLowerCase()) {
+            case "google":
+                accessToken = new GoogleAuthorizationCodeTokenRequest(
+                        new NetHttpTransport(), new GsonFactory(),
+                        googleClientId,
+                        googleClientSecret,
+                        code,
+                        googleRedirectUri
+                ).execute().getAccessToken();
+
+                // Configure RestTemplate to include the access token in the Authorization header
+                restTemplate.getInterceptors().add((req, body, executionContext) -> {
+                    req.getHeaders().set("Authorization", "Bearer " + accessToken);
+                    return executionContext.execute(req, body);
+                });
+
+                // Make a GET request to fetch user information
+                return new ObjectMapper().readValue(
+                        restTemplate.getForEntity("googleUserInfoUri", String.class).getBody(),
+                        new TypeReference<>() {});
+            //break;
+
+            case "facebook":
+                // Facebook token request setup
+                String urlGetAccessToken = UriComponentsBuilder
+                        .fromUriString("facebookTokenUri")
+                        .queryParam("client_id", "facebookClientId")
+                        .queryParam("redirect_uri", "facebookRedirectUri")
+                        .queryParam("client_secret", "facebookClientSecret")
+                        .queryParam("code", code)
+                        .toUriString();
+
+                ResponseEntity<String> response = restTemplate.getForEntity(urlGetAccessToken, String.class);
+                ObjectMapper mapper = new ObjectMapper();
+                JsonNode node = mapper.readTree(response.getBody());
+                accessToken = node.get("access_token").asText();
+
+
+                String userInfoUri = "facebookUserInfoUri" + "&access_token=" + accessToken;
+                return mapper.readValue(
+                        restTemplate.getForEntity(userInfoUri, String.class).getBody(),
+                        new TypeReference<>() {});
+
+            default:
+                System.out.println("Unsupported login type: " + loginType);
+                return null;
+        }
+    }
     public UserDetailsService getUserDetailsService() {
         return username -> userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException(username));
