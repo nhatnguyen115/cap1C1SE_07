3b973f5297dd76a1d1bca63d924a79008c494691
nameless1908 <huytech1908@gmail.com>
2025-05-12 16:40:54 +0700

    rollback PartDetailsResponse

--- BE/src/main/java/com/arkdev/z9tkvtu/dto/Response/PartDetailsResponse.java
+++ BE/src/main/java/com/arkdev/z9tkvtu/dto/Response/PartDetailsResponse.java
@@ -10,5 +10,5 @@ import java.util.List;
  */
 public record PartDetailsResponse<T>(
         PartResponse part,
-        List<T> list) implements Serializable {
+        List<T> questions) implements Serializable {
 }
